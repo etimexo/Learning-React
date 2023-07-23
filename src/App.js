05d0fcf16bdc2f42a15a6c5c11d79390e2355a46
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-07-23 22:15:24 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+function App () {
+  const planets = [
+    { name: "Mars", isGasPlanet: false},
+    { name: "Earth", isGasPlanet: false},
+    { name: "Jupiter", isGasPlanet: true},
+    { name: "Venus", isGasPlanet: false},
+    { name: "Neptune", isGasPlanet: true},
+    { name: "Uranus", isGasPlanet: true},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {planets.map((props, key) => {
+        return (
+          <h1 key={key}>{props.isGasPlanet ? props.name + "- True" : props.name + "- False"}</h1>
+        )
+      })}
     </div>
-  );
+  )
 }
-
 export default App;
